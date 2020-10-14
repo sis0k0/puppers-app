8705fc0e581d5a108a0eb65b6f41cf1cec9ef888
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2020-10-14 11:31:39 +0300

    Extract route param

--- src/app/dogs-list/dogs-list.component.ts
+++ src/app/dogs-list/dogs-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dogs-list',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const breed = this.route.snapshot.paramMap.get('breed');
+    alert(breed);
   }
 
 }
