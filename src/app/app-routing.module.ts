b9f55acf6885e441ad105db6e0adc6835292d945
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2020-10-14 11:29:07 +0300

    Routing to list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DogsListComponent } from './dogs-list/dogs-list.component';
+import { DogFormComponent } from './dog-form/dog-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DogFormComponent },
+  { path: 'list/:breed', component: DogsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DogFormComponent } from './dog-form/dog-form.component';
+import { DogsListComponent } from './dogs-list/dogs-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DogFormComponent
+    DogFormComponent,
+    DogsListComponent
   ],
   imports: [
     BrowserModule,
